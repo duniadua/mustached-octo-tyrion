c8b455b2bbf8c091dd05d0129a9c3ca1a3f717b9
duniadua <duniadua@gmail.com>
2013-04-28 23:46:28 +0700

    footer add icon

--- application/views/footer.php
+++ application/views/footer.php
@@ -10,15 +10,15 @@
             <div class="span4">
                 <div class="clearfix"></div>
                 <address>
-                    <i class="icon-map-marker"></i><strong>Twitter, Inc.</strong><br>
-                    795 Folsom Ave, Suite 600<br>
+                    <strong>Twitter, Inc.</strong><br>
+                    <i class="icon-map-marker"></i> 795 Folsom Ave, Suite 600<br>
                     San Francisco, CA 94107<br>
                     <abbr title="Phone">P:</abbr> (123) 456-7890
                 </address>
 
                 <address>
                     <strong>Full Name</strong><br>
-                    <i class="icon-map-marker"></i><a href="mailto:#">first.last@example.com</a>
+                    <i class="icon-envelope"></i><a href="mailto:#"> first.last@example.com</a>
                 </address>
             </div>
         </div>
