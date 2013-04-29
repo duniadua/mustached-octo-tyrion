63fba40e7cf50235ce1a93968e3e126c754512cc
duniadua <duniadua@gmail.com>
2013-04-29 13:39:38 +0700

    new pict 640*400

--- application/views/body.php
+++ application/views/body.php
@@ -14,14 +14,14 @@
                 </ol>
                 <div class="carousel-inner">
                     <div class="item active">
-                        <img src="<?php echo base_url(); ?>images/portfolio/original-540x400.jpg" alt="">
+                        <img src="<?php echo base_url(); ?>images/portfolio/porto_klink.jpg" alt="">
                     </div>
                     <div class="item">
-                        <img src="<?php echo base_url(); ?>images/portfolio/EDWARD-SHARPE-540x389.jpg" alt="">                    
+                        <img src="<?php echo base_url(); ?>images/portfolio/porto_klink_new.jpg" alt="">                    
                     </div>
                 </div>
-<!--                <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-                <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>-->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
 
             </div>
         </div>
