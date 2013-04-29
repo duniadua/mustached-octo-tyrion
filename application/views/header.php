75aedb270735ce84591e2f3371a6444be993f41a
duniadua <duniadua@gmail.com>
2013-04-29 17:48:41 +0700

    update link

--- application/views/header.php
+++ application/views/header.php
@@ -3,6 +3,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+if($active =='y'):
+    $active = "class='active'";
+else:
+    $active = " ";
+endif;
+
+
 ?>
 <div class="container">
     <div class="line"></div>
@@ -10,11 +17,14 @@
     <div class="row-fluid">
         <div class="span11">
             <ul class="nav nav-tabs">
-            <li class="active">
+                <li <?php echo $active; ?>>
                 <a href="<?php echo site_url('/home')?>"> Home</a>
             </li>
-            <li><a href="<?php echo site_url('/promo')?>"> Promo</a></li>
-            <li><a href="<?php echo site_url('/news')?>"> News</a></li>
+            <li <?php echo $active; ?>><a href="<?php echo site_url('/promo')?>"> Promo</a></li>
+            <li <?php // echo $active; ?>><a href="<?php echo site_url('/news')?>"> News</a></li>
+            <li <?php // echo $active; ?>><a href="<?php echo site_url('/syariah')?>"> Syariah</a></li>
+            <li <?php // echo $active; ?>><a href="<?php echo site_url('/gallery')?>"> Gallery</a></li>
+            <li <?php // echo $active; ?>><a href="<?php echo site_url('/community')?>"> Community</a></li>
             <li><a href="#">Signin</a></li>
         </ul>
         </div>
