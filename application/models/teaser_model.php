74ae22b1446e237ccf3a8117ab6be32e6e767465
duniadua <duniadua@gmail.com>
2013-04-30 16:22:29 +0700

    teaser model

---
+++ application/models/teaser_model.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of teaser_model
+ *
+ * @author sahid
+ */
+class teaser_model extends CI_Model {
+
+    //put your code here
+    public function add() {
+        $data = array(
+            'duration' => $this->input->post('duration'),
+            'direction' => $this->input->post('direction'),
+            'active' => $this->input->post('active'),
+            'create_dt' => date('Y-m-d H:i:s'),
+            'ip_address' => $this->input->ip_address(),
+        );
+
+        $this->db->insert('teaser', $data);
+    }
+
+}
+
+?>
