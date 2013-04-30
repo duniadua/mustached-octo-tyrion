7c7ea91eaa3d3690ea83883afe40c60165549878
duniadua <duniadua@gmail.com>
2013-04-30 16:25:29 +0700

    update upload

--- application/controllers/teaser.php
+++ application/controllers/teaser.php
@@ -10,33 +10,53 @@
  *
  * @author sahid
  */
-class Teaser extends CI_Controller{
+class Teaser extends CI_Controller {
+
     //put your code here
-    public function Teaser(){
+    public function Teaser() {
         parent::__construct();
     }
-    
-    public function index(){
+
+    public function index() {
         $data = array(
             'error' => '',
         );
-        
+
         $this->load->view('html_config_common');
         $this->load->view('header_cp');
-        $this->load->view('upload_teaser',$data);
+        $this->load->view('upload_teaser', $data);
         $this->load->view('footer_cp');
     }
-    
-    public function do_upload(){
+
+    public function do_upload() {
         $config = array(
-            'upload_path' => FCPATH.'/images/portfolio/',
+            'upload_path' => FCPATH . '/images/portfolio/',
             'allowed_types' => 'gif|jpg|png',
             'max_size' => 700,
             'remove_spaces' => TRUE,
         );
-        
+
         $this->upload->initialize($config);
-        
+
+        if (!$this->upload->do_upload()):
+            $data = array('error' => $this->upload->display_errors());
+
+            $this->load->view('html_config_common');
+            $this->load->view('header_cp');
+            $this->load->view('upload_teaser', $data);
+            $this->load->view('footer_cp');
+        else:
+            $data = array(
+                'type' => 'Uploaded',
+                'mssg' => 'File Uploaded',
+            );
+            
+            $this->load->view('html_config_common');
+            $this->load->view('header_cp');
+            $this->load->view('success', $data);
+            $this->load->view('footer_cp');
+        endif;
     }
+
 }
 
