b972eea42ce9ac21f67aabc0d2044638df47941b
duniadua <duniadua@gmail.com>
2013-04-30 15:30:31 +0700

    modif upload

--- application/controllers/teaser.php
+++ application/controllers/teaser.php
@@ -13,17 +13,29 @@
 class Teaser extends CI_Controller{
     //put your code here
     public function Teaser(){
-        
+        parent::__construct();
     }
     
     public function index(){
+        $data = array(
+            'error' => '',
+        );
+        
         $this->load->view('html_config_common');
         $this->load->view('header_cp');
-        $this->load->view('upload_teaser');
+        $this->load->view('upload_teaser',$data);
         $this->load->view('footer_cp');
     }
     
     public function do_upload(){
+        $config = array(
+            'upload_path' => FCPATH.'/images/portfolio/',
+            'allowed_types' => 'gif|jpg|png',
+            'max_size' => 700,
+            'remove_spaces' => TRUE,
+        );
+        
+        $this->upload->initialize($config);
         
     }
 }
