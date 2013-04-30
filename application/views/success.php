f53157c9a42d5aa59dbeb7668630848573c95a30
duniadua <duniadua@gmail.com>
2013-04-30 16:17:03 +0700

    update mssges

---
+++ application/views/success.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<div class="container">
+    <div class="alert alert-success">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong><?php echo $type?></strong> <?php echo $mssg; ?>.
+    </div>
+</div>
+
