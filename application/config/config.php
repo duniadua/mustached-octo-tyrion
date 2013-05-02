8674283a19d8e0460a9b9622d63c41e1ca5669cb
duniadua <duniadua@gmail.com>
2013-05-02 15:42:31 +0700

    config key

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'OrKfLgT5rrUx4C3k5h4I7GL7wnKN9Tsx';
 
 /*
 |--------------------------------------------------------------------------
