e1078e5ebf3772e66b951fe810b2f93e16a5a29f
duniadua <duniadua@gmail.com>
2013-05-02 17:49:56 +0700

    add migrate controller

---
+++ application/controllers/migrate.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of migrate
+ *
+ * @author sahid
+ */
+class migrate extends CI_Controller {
+
+//put your code here
+    public function index($ver) {
+        $this->load->library('migration');
+
+        if (!$this->migration->version($ver)):
+            show_error($this->migration->error_string());
+        else:
+            echo 'migration success -'.$ver.br();
+        endif;
+            
+        
+    }
+
+}
