2206c4b8fb4c2f2b11370f03b49f66e441398026
duniadua <duniadua@gmail.com>
2013-05-02 17:47:37 +0700

    fix js

--- application/controllers/control_panel.php
+++ application/controllers/control_panel.php
@@ -13,7 +13,7 @@
 class Control_panel extends CI_Controller{
     //put your code here
     public function index(){
-        $this->load->view('html_config_common');
+        $this->load->view('html_config_min');
         $this->load->view('header_cp');
         $this->load->view('home_cp');
         $this->load->view('footer_cp');
