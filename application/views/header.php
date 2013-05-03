1255e74de95b578561cd803b1602c41380521062
duniadua <duniadua@gmail.com>
2013-05-03 22:47:34 +0700

    delete span11 agar css menu fit

--- application/views/header.php
+++ application/views/header.php
@@ -13,21 +13,18 @@ endif;
 ?>
 <div class="container">
     <div class="line"></div>
-    <div class="skyline"></div>
     <div class="row-fluid">
-        <div class="span11">
-            <ul class="nav nav-tabs">
+            <ul class="nav nav-pills cssmenus">
                 <li <?php echo $active; ?>>
                 <a href="<?php echo site_url('/home')?>"> Home</a>
             </li>
-            <li <?php echo $active; ?>><a href="<?php echo site_url('/promo')?>"> Promo</a></li>
+            <li <?php // echo $active; ?>><a href="<?php echo site_url('/promo')?>"> Promo</a></li>
             <li <?php // echo $active; ?>><a href="<?php echo site_url('/news')?>"> News</a></li>
             <li <?php // echo $active; ?>><a href="<?php echo site_url('/syariah')?>"> Syariah</a></li>
             <li <?php // echo $active; ?>><a href="<?php echo site_url('/gallery')?>"> Gallery</a></li>
             <li <?php // echo $active; ?>><a href="<?php echo site_url('/community')?>"> Community</a></li>
             <li><a href="#">Signin</a></li>
         </ul>
-        </div>
     </div>
 </div>
 
